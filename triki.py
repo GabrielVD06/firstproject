04fe647a75aee38c202b7c10d0805beb8f7696a6
Gabriel Villota <villota.gabo@gmail.com>
2026-04-23 16:13:43 -0500

    Uppdate

---
+++ triki.py
@@ -0,0 +1,93 @@
+import os
+
+def checkPos(check: list, fila: int, columna: int) -> bool :
+    base = False
+    if str(check[fila][columna]) == "[-]":
+        base = True
+    return(base)
+
+def winner(rev: list, fila: int, columna: int) -> bool :
+    result = False
+    if rev[fila][columna] == matrix[0][0,3] :
+        result = True
+    return(result)
+
+rows = 3
+col = 3
+
+matrix = [] 
+os.system('cls')
+for i in range(rows):   
+    row = []
+    for j in range(col):
+        row.append("[-]")   
+    matrix.append(row)  
+
+print("triki matrix")
+
+for i in range(rows):
+    for j in range(col):
+        print(matrix[i][j], end=" ")
+    print()
+fila = 0
+columna = 0
+count = 0
+user = 1
+while count < 9 and winner(matrix, fila, columna) == True:
+    for i in range(rows):   
+        row = []
+        for j in range(col):
+            row.append("[-]")   
+        matrix.append(row)
+
+    if user == 1:
+        playerName = "Player 1"
+    if user == 2:
+        playerName = "Player 2"
+
+    fila = int(str(input(playerName + " introduce the row: ")))
+    while fila < 0 or fila > 2 :
+        #os.system('cls')
+        fila = int(str(input(playerName + " introduce a possible row: ")))
+
+    columna = int(str(input(playerName + " introduce the col: ")))
+    while columna < 0 or columna > 2 :
+            #os.system('cls')
+        columna = int(str(input(playerName + " introduce a possible col: ")))
+    
+         
+
+
+    while checkPos(matrix, fila, columna) == False:
+        print("triki matrix")
+        for x in range(rows):
+            for y in range(col):
+                print(matrix[x][y], end=" ")
+            print()
+        fila = int(str(input(playerName +  " introduce another row: ")))
+        while fila < 0 or fila > 2 :
+                #os.system('cls')
+            fila = int(str(input(playerName + " introduce another row: ")))
+
+        columna = int(str(input(playerName + " introduce another col: ")))
+        while columna < 0 or columna > 2 :
+                #os.system('cls')
+            columna = int(str(input(playerName + " introduce another col: ")))
+    
+    os.system('cls')
+    if user == 1:
+        matrix[fila][columna] = "[x]"
+        user = 2
+    elif user == 2:
+        matrix[fila][columna] = "[O]"
+        user = 1
+            
+
+    print("triki matrix")
+    for x in range(rows):
+        for y in range(col):
+            print(matrix[x][y], end=" ")
+        print()
+    count += 1
+
+    
