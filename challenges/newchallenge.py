b493caf31a2ebac724fea74a3ff5d00279e6cc5d
Gabriel Villota <villota.gabo@gmail.com>
2026-08-14 12:49:36 -0500

    new challenges

---
+++ challenges/newchallenge.py
@@ -0,0 +1,8 @@
+count = 0
+for i in range(1, int(input())): count = (count*10 + 1) ; print((count**2))
+
+for i in range(1, int(input())): # More than 2 lines cannot be used. Do not change this line.
+    print(i * (10**i - 1) // 9)
+
+for i in range(1, int(input())): # More than 2 lines cannot be used. Do not change this line.
+    print(i * (10**i - 1) // 9)
